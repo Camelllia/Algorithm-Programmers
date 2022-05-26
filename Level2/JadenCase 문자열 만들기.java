aa8873a68755ab73452a2a03f96f03a92fcc298d
KimSeungJin <dahyun101107@naver.com>
2022-05-26 13:58:29 +0900

    Update JadenCase 문자열 만들기.java

--- Level2/JadenCase 문자열 만들기.java
+++ Level2/JadenCase 문자열 만들기.java
@@ -1,2 +1,38 @@
-public class dd {
+class Solution {
+    public String solution(String s) {
+
+        String answer = "";
+        boolean flag = false;
+
+        if(s.charAt(s.length() - 1) == ' ') {
+            flag = true;
+        }
+
+        String[] str =  s.split(" ");
+
+        for(int i = 0; i < str.length; i++) {
+
+            StringBuilder sb = new StringBuilder(str[i]);
+
+            for(int j = 0; j < sb.length(); j++) {
+                if(j == 0) {
+                    if(Character.isDigit(sb.charAt(0))) {
+                        sb.setCharAt(1, Character.toUpperCase(sb.charAt(1)));
+                    } else {
+                        sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+                    }
+                } else {
+                    sb.setCharAt(j, Character.toLowerCase(sb.charAt(j)));
+                }
+            }
+
+            if(i == str.length - 1 && flag == false) {
+                answer += sb;
+            } else {
+                answer += sb + " ";
+            }
+        }
+
+        return answer;
+    }
 }
