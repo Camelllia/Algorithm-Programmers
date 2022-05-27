ef8e794ceb2cb95968d3f41226074edfa36f7d87
KimSeungJin <dahyun101107@naver.com>
2022-05-27 16:08:14 +0900

    Update 네트워크.java

--- Level3/네트워크.java
+++ Level3/네트워크.java
@@ -1,2 +1,29 @@
-public class dd {
+class Solution {
+
+    boolean[] visited = new boolean[201];
+
+    public int solution(int n, int[][] computers) {
+
+        int answer = 0;
+
+        for(int i = 0; i < computers.length; i++) {
+            if(!visited[i]) {
+                answer++;
+                DFS(i, computers, computers.length);
+            }
+        }
+
+        return answer;
+    }
+
+    void DFS(int n, int[][] computers, int size) {
+
+        visited[n] = true;
+
+        for(int i = 0; i < size; i++) {
+            if(!visited[i] && computers[n][i] == 1) {
+                DFS(i, computers, size);
+            }
+        }
+    }
 }
